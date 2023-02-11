eedf417dd229da1b589348dc839f5ff4a0796f47
salahHaciAkil <your_email@abc.com>
2023-02-11 11:44:57 +0400

    adding env.ts file

---
+++ backend/src/config/env.ts
@@ -0,0 +1,7 @@
+import "dotenv/config";
+
+export const { DB_USER } = process.env;
+export const { DB_HOST } = process.env;
+export const { DB_DATABASE } = process.env;
+export const { DB_PASSWORD } = process.env;
+export const { DB_PORT } = process.env;
