21f61cde5686ba0e85099712cfd42cc9fb9fe259
salahHaciAkil <your_email@abc.com>
2023-02-12 16:07:46 +0400

    init front-end

---
+++ frontend/src/App.js
@@ -0,0 +1,25 @@
+import { Route, Routes } from "react-router-dom";
+import "./App.scss";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import MainContainer from "./components/MainContainer";
+import { ROUTES } from "./constants/routes";
+function App() {
+  return (
+    <>
+      <MainContainer>
+        <Navbar />
+        <Sidebar />
+        <div className="wrapper">
+          <Routes>
+            {ROUTES.map(({ element: Element, ...rest }) => (
+              <Route {...rest} element={<Element />} />
+            ))}
+          </Routes>
+        </div>
+      </MainContainer>
+    </>
+  );
+}
+
+export default App;
