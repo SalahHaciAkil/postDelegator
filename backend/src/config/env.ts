ec15f816b598c394a5324d48d3e29c3f79c10521
salahHaciAkil <your_email@abc.com>
2023-02-19 16:52:37 +0300

    adding PORT

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -5,3 +5,4 @@ export const { DB_HOST } = process.env;
 export const { DB_DATABASE } = process.env;
 export const { DB_PASSWORD } = process.env;
 export const { DB_PORT } = process.env;
+export const { PORT } = process.env;
