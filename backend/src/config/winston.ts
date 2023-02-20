4f51c7977e369b3ddb125ee2f19bdb9661b9b671
salahHaciAkil <your_email@abc.com>
2023-02-20 14:30:29 +0300

    updating winston destination logs

--- backend/src/config/winston.ts
+++ backend/src/config/winston.ts
@@ -1,28 +1,33 @@
-import winston from 'winston';
+import winston from "winston";
 
 const logger = winston.createLogger({
-    level: 'info',
-    format: winston.format.combine(
-        winston.format.timestamp({
-        format: 'YYYY-MM-DD HH:mm:ss'
-      }),
-      winston.format.errors({ stack: true }),
-      winston.format.splat(),
-      winston.format.json()
-    ),
-    transports: [
-      new winston.transports.File({ filename: 'logs/errors.log', level: 'error' }),
-      new winston.transports.File({ filename: 'logs/app.log' })
-    ]
-  });
-  
-  if (process.env.NODE_ENV !== 'production') {
-    logger.add(new winston.transports.Console({
+  level: "info",
+  format: winston.format.combine(
+    winston.format.timestamp({
+      format: "YYYY-MM-DD HH:mm:ss",
+    }),
+    winston.format.errors({ stack: true }),
+    winston.format.splat(),
+    winston.format.json()
+  ),
+  transports: [
+    new winston.transports.File({
+      filename: "appLogs/errors.log",
+      level: "error",
+    }),
+    new winston.transports.File({ filename: "appLogs/app.log" }),
+  ],
+});
+
+if (process.env.NODE_ENV !== "production") {
+  logger.add(
+    new winston.transports.Console({
       format: winston.format.combine(
         winston.format.colorize(),
         winston.format.simple()
-      )
-    }));
-  }
+      ),
+    })
+  );
+}
 
-  export default logger;
+export default logger;
